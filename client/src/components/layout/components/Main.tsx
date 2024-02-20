c58745b025048d1d53a50f8457f249b3200ac043
Cosimo Curlante <cosimocurlante8@gmail.com>
2024-02-20 16:07:54 +0100

    client fixes

--- client/src/components/layout/components/Main.tsx
+++ client/src/components/layout/components/Main.tsx
@@ -1,11 +1,7 @@
-import Gets from "../../pages/Gets";
-
 const Main = () => {
 	return (
 		<div className="h-[90vh] py-5 px-5">
-			<div className="h-full">
-				<Gets />
-			</div>
+			<div className="h-full"></div>
 		</div>
 	);
 };
