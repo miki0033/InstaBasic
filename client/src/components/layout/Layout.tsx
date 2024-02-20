24518747d99204e69aff1aa538fa8eb5ccc9f256
Cosimo Curlante <cosimocurlante8@gmail.com>
2024-02-20 17:24:15 +0100

    Client: [Route, DataProvider], [Pages: PageNotFound]

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -2,11 +2,13 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
+import { Outlet } from "react-router-dom";
+
 const Layout = () => {
 	return (
 		<div className="h-[100vh] bg-default-50 text-center text-default-600">
 			<Header />
-			<Main />
+			<Main children={<Outlet />} />
 			<Footer />
 		</div>
 	);
--- client/src/components/main/App.tsx
+++ client/src/components/main/App.tsx
@@ -1,7 +1,36 @@
+import { Routes, Route } from "react-router-dom";
+
 import Layout from "../layout/Layout";
 
+import PageNotFound from "../../utils/PageNotFound";
+
 function App() {
-	return <Layout />;
+	return (
+		<>
+			{/* prettier-ignore */}
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<>Home</>} />
+
+					<Route path="feed">
+						<Route index element={<>Feed</>} />
+					</Route>
+
+					<Route path="profile">
+						<Route index element={<>Profile</>}/>
+
+						<Route path="settings">
+							<Route index element={<>Settings</>}/>
+						</Route>
+					</Route>
+
+					
+				</Route>
+
+				<Route path="*" element={<PageNotFound/>} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
