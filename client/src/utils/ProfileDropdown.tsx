7151cba06ad15b600452ee30a308c49aeb615767
Cosimo Curlante <cosimocurlante8@gmail.com>
2024-02-23 14:14:38 +0100

    Client: [Layout update]

--- client/src/utils/ProfileDropdown.tsx
+++ client/src/utils/ProfileDropdown.tsx
@@ -1,23 +1,35 @@
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar } from "@nextui-org/react";
+import { useData } from "../components/main/DataProvider";
+import { Link } from "react-router-dom";
 
 const ProfileDropdown = () => {
+	const {
+		state: { user, profile },
+	} = useData();
 	return (
 		<Dropdown placement="bottom-start" className="border border-1 border-secondary-200">
 			<DropdownTrigger>
-				<Avatar color="primary" className="w-10 h-10 my-auto mr-20" />
+				<Avatar color="primary" className="w-10 h-10 my-auto mr-20" src={profile?.avatarUrl ? profile.avatarUrl : ""} />
 			</DropdownTrigger>
 
 			<DropdownMenu aria-label="Profile Actions" variant="flat">
 				<DropdownItem key="profile" className="h-14 gap-2">
 					<p className="font-semibold">Signed in as</p>
-					<p className="font-semibold">zoey@example.com</p>
+					<p className="font-semibold">{user?.email}</p>
 				</DropdownItem>
-				<DropdownItem key="settings">My Settings</DropdownItem>
-				<DropdownItem key="team_settings">Team Settings</DropdownItem>
-				<DropdownItem key="analytics">Analytics</DropdownItem>
-				<DropdownItem key="system">System</DropdownItem>
-				<DropdownItem key="configurations">Configurations</DropdownItem>
-				<DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+
+				<DropdownItem key="profile">
+					<Link to={"/profile"}>
+						<p>Profile</p>
+					</Link>
+				</DropdownItem>
+
+				<DropdownItem key="settings">
+					<Link to={"/profile/settings"}>
+						<p>Settings</p>
+					</Link>
+				</DropdownItem>
+
 				<DropdownItem key="logout" color="danger">
 					Log Out
 				</DropdownItem>
