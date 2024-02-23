8b74be8e330ace9c9bc2d6e4b80d93b83d504696
Cosimo Curlante <cosimocurlante8@gmail.com>
2024-02-23 14:14:22 +0100

    Client: [Layout update]

--- client/src/components/layout/components/Footer.tsx
+++ client/src/components/layout/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<div className="h-[4vh] rounded-t-3xl bg-primary-200 flex flex-col justify-center">
+		<div className="h-[4vh] rounded-t-3xl bg-secondary-200 flex flex-col justify-center">
 			<h1 className="w-96 text-sm ml-auto mr-5">developed by Michele Monteferrante and Cosimo Curlante</h1>
 		</div>
 	);
