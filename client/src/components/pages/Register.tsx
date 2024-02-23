59a59f40b159a24a17a1eed781f0b84aa23197a1
Cosimo Curlante <cosimocurlante8@gmail.com>
2024-02-23 14:15:26 +0100

    Client: [New Pages: (Login, Register) ]

---
+++ client/src/components/pages/Register.tsx
@@ -0,0 +1,49 @@
+import { Button, Input, ScrollShadow } from "@nextui-org/react";
+
+const Register = () => {
+	return (
+		<div className="w-full h-full flex flex-col justify-center">
+			<div className="w-8/12 mx-auto h-full flex flex-col justify-around rounded-md bg-secondary-200">
+				<h1 className="text-3xl ">Register</h1>
+
+				<div className="h-5/6 flex flex-row justify-around">
+					<ScrollShadow className="w-7/12 h-full my-auto pt-10 flex flex-col gap-5" hideScrollBar size={5}>
+						<div className="flex flex-row gap-5 justify-center">
+							<Input label="Name" labelPlacement="outside" color="primary" className="w-5/12" />
+							<Input label="Surname" labelPlacement="outside" color="primary" className="w-5/12" />
+						</div>
+
+						<div className="flex flex-row gap-5 justify-center">
+							<Input label="Username" labelPlacement="outside" color="primary" className="w-5/12" />
+						</div>
+
+						<div>
+							<div className="flex flex-row gap-5 justify-center mb-2">
+								<Input label="E-mail" labelPlacement="outside" color="primary" className="w-5/12" />
+								<Input label="Confirm E-mail" labelPlacement="outside" color="primary" className="w-5/12" />
+							</div>
+
+							<div className="flex flex-row gap-5 justify-center">
+								<Input label="Password" type="password" labelPlacement="outside" color="primary" className="w-5/12" />
+								<Input label="Confirm Password" type="password" labelPlacement="outside" color="primary" className="w-5/12" />
+							</div>
+						</div>
+
+						<div className="flex flex-row gap-5 justify-center">
+							<Input label="Birthday" type="date" labelPlacement="outside-left" color="primary" className="w-5/12" />
+						</div>
+
+						<Button className="w-20 mx-auto my-10" color="primary">
+							Register
+						</Button>
+					</ScrollShadow>
+					<div className="w-4/12 h-4/5 my-auto flex flex-col justify-center">
+						<img src="/logo.png" alt="Instabasic Logo" className="w-10/12 m-auto" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Register;
