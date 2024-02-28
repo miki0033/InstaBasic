b2c4c124146fb6aaeae57f0b62674bc405b07820
Cosimo Curlante <cosimocurlante8@gmail.com>
2024-02-28 15:27:04 +0100

    Client: [Feed fixes]

--- client/src/components/pages/feed/Carousel.tsx
+++ client/src/components/pages/feed/Carousel.tsx
@@ -3,15 +3,11 @@ import { PlusCircleIcon } from "@heroicons/react/24/outline";
 
 const Carousel = () => {
 	return (
-		<div className="w-full h-full">
-			<div className="w-full h-full bg-red-500 flex flex-row gap-5 px-10 overflow-auto scrollbar-hide">
+		<div className="w-full h-full py-2">
+			<div className="w-3/4 h-24 mx-auto bg-red-500 rounded-md flex flex-row gap-5 px-5 overflow-auto scrollbar-hide">
 				<Button isIconOnly className="w-16 h-16 my-auto rounded-full flex-none" color="success">
 					<PlusCircleIcon />
 				</Button>
-				<Avatar isBordered color="success" src="" className="w-16 h-16 my-auto flex-none" />
-				<Avatar isBordered color="success" src="" className="w-16 h-16 my-auto flex-none" />
-				<Avatar isBordered color="success" src="" className="w-16 h-16 my-auto flex-none" />
-				<Avatar isBordered color="success" src="" className="w-16 h-16 my-auto flex-none" />
 				<Avatar isBordered color="success" src="" className="w-16 h-16 my-auto flex-none" />
 				<Avatar isBordered color="success" src="" className="w-16 h-16 my-auto flex-none" />
 				<Avatar isBordered color="success" src="" className="w-16 h-16 my-auto flex-none" />
--- client/src/components/pages/feed/Feed.tsx
+++ client/src/components/pages/feed/Feed.tsx
@@ -4,8 +4,8 @@ const Feed = () => {
 	return (
 		<div className=" w-full h-full flex flex-row">
 			<div className="w-1/5 bg-secondary-300 rounded-l-md">WIP</div>
-			<div className="w-3/5 bg-primary-300 rounded-t-md">
-				<div className="w-full h-28 bg-primary-400">
+			<div className="w-3/5 bg-primary-300">
+				<div className="w-full h-32 ">
 					<Carousel />
 				</div>
 				Feed
