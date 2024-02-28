9c61d01404a6951311e5c31d9f431f3d04619a02
Cosimo Curlante <cosimocurlante8@gmail.com>
2024-02-28 15:18:44 +0100

    Resources: [fixes ]

--- resources/start.py
+++ resources/start.py
@@ -11,13 +11,23 @@ def index():
 
 @app.route('/pfp/get/<profileImageName>')
 def get_PFP(profileImageName):
-    return send_file("pfp\\"+profileImageName, mimetype="image")
+    return send_file("pfp/"+profileImageName, mimetype="image")
 
 
 @app.route('/pfp/post/<profileImageName>')
 def post_PFP(profileImageName):
-    return send_file("pfp\\"+profileImageName, mimetype="image")
+    return "WIP"
+
+
+@app.route('/post/get/<postResourceName>')
+def get_Post(postResourceName):
+    return send_file("posts/"+postResourceName, mimetype="image")
+
+
+@app.route('/post/post/<postResourceName>')
+def post_Post(postResourceName):
+    return "WIP"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='localhost', port=4000, debug=True)
