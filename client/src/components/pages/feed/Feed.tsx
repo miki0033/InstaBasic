b36c3ec6a4efc78086289777a10d27d53fff0f6e
Cosimo Curlante <cosimocurlante8@gmail.com>
2024-03-04 16:23:06 +0100

    Client: [Pages.Feed update/fixes]

--- client/src/components/pages/feed/Feed.tsx
+++ client/src/components/pages/feed/Feed.tsx
@@ -3,14 +3,13 @@ import Carousel from "./Carousel";
 const Feed = () => {
 	return (
 		<div className=" w-full h-full flex flex-row">
-			<div className="w-1/5 bg-secondary-300 rounded-l-md">WIP</div>
-			<div className="w-3/5 bg-primary-300">
+			<div className="w-1/5 bg-content2 border-2 border-secondary-400 border-r-divider rounded-l-md"></div>
+			<div className="w-3/5 bg-content2 border-2 border-primary-400 border-x-divider">
 				<div className="w-full h-32 ">
 					<Carousel />
 				</div>
-				Feed
 			</div>
-			<div className="w-1/5 bg-secondary-300 rounded-r-md">Notification</div>
+			<div className="w-1/5 bg-content2 border-2 border-secondary-400 border-l-divider rounded-r-md"></div>
 		</div>
 	);
 };
