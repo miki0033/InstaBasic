e39334834bc58aa9d88e602cd8f33063cdd74c07
Cosimo Curlante <cosimocurlante8@gmail.com>
2024-03-05 16:30:52 +0100

    Client: [Pages.Profile(.Info, .Posts(.PostCard, .AddPostCard ) ) created/updated ]

---
+++ client/src/components/pages/profile/PostCard.tsx
@@ -0,0 +1,82 @@
+import { Modal, ModalContent, ModalBody, ModalFooter, Button, useDisclosure, Pagination, ModalHeader, Avatar } from "@nextui-org/react";
+import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import { HeartIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { useData } from "../../main/DataProvider";
+
+const PostCard = ({ post }: { post: IPost }) => {
+	const { isOpen, onOpen, onOpenChange } = useDisclosure();
+	const [modalImagePage, nextImage] = useState(0);
+	const {
+		state: { profile },
+	} = useData();
+
+	return (
+		<>
+			<Card
+				shadow="sm"
+				isPressable
+				onPress={() => {
+					onOpen();
+					nextImage(0);
+				}}
+				className="w-60 h-60 border border-secondary-400">
+				<CardBody>
+					<Image shadow="sm" radius="lg" width="100%" alt={post.title} className="w-full object-cover h-[140px]" src={post.url[0]} />
+				</CardBody>
+				<CardFooter className="text-small">
+					<p className="w-8/12">{post.title}</p>
+					<Button className="w-4/12 text-default-500">
+						{post.likes} <HeartIcon />
+					</Button>
+				</CardFooter>
+			</Card>
+
+			<Modal isOpen={isOpen} onOpenChange={onOpenChange} size="5xl" backdrop="blur" className="my-10">
+				<ModalContent className=" border-2 border-secondary-400">
+					{(onClose) => (
+						<>
+							<ModalHeader />
+							<ModalBody className="p-0">
+								<div className="w-full h-full flex flex-row flex-wrap">
+									<div className="w-7/12 my-2 px-3 py-5 flex flex-col gap-10">
+										<div className="mx-auto px-3">
+											<Image alt={post.title} isBlurred className="max-h-[50vh] object-cover" src={post.url[modalImagePage]} />
+										</div>
+
+										<Pagination
+											className=" mx-auto"
+											total={post.url.length}
+											initialPage={1}
+											onChange={(pages) => {
+												nextImage(pages - 1);
+											}}
+										/>
+									</div>
+
+									<div className="w-5/12 my-10 px-5">
+										<h1 className="py-3 text-center text-4xl ">{post.title}</h1>
+										<p className="flex flex-row gap-1">
+											<div className="w-auto flex flex-row gap-1">
+												<Avatar isBordered src={profile?.avatarUrl} className="w-6 h-6" />
+												<p className="text-primary-500">{profile?.userName + ":"}</p>
+											</div>
+											<div className="text-default-800">{post.description}</div>
+										</p>
+									</div>
+								</div>
+							</ModalBody>
+							<ModalFooter>
+								<Button color="danger" variant="light" onPress={onClose}>
+									Close
+								</Button>
+							</ModalFooter>
+						</>
+					)}
+				</ModalContent>
+			</Modal>
+		</>
+	);
+};
+
+export default PostCard;
--- client/src/components/pages/profile/ProfilePosts.tsx
+++ client/src/components/pages/profile/ProfilePosts.tsx
@@ -1,3 +1,22 @@
+import { useData } from "../../main/DataProvider";
+import AddPostCard from "./AddPostCard";
+import PostCard from "./PostCard";
+
 export const ProfilePosts = () => {
-	return <div className="pt-10 pb-5">ProfilePosts</div>;
+	const {
+		state: { posts },
+	} = useData();
+
+	return (
+		<div className="w-full h-full px-14 pt-10 pb-5 flex flex-row flex-wrap justify-between gap-10">
+			<AddPostCard />
+			{posts.map((el) => {
+				return (
+					<>
+						<PostCard post={el} />
+					</>
+				);
+			})}
+		</div>
+	);
 };
